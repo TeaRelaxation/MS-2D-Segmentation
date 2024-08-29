d91db5cb91fe7c3c10b3e736ad23552742b0919e
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-08-29 23:53:37 +0330

    Add data augmentation

---
+++ dataloaders/transform.py
@@ -0,0 +1,23 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from torchvision import transforms
+
+
+def get_augmentor(mode):
+    if mode == "train":
+        return A.Compose([
+            A.Resize(height=217, width=181, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.Rotate(limit=10, p=0.5),
+            ToTensorV2(),
+        ])
+    elif mode == "test":
+        return A.Compose([
+            A.Resize(height=217, width=181, p=1.0),
+            ToTensorV2(),
+        ])
+    return None
+
+
+def get_normalizer(mean, std):
+    return transforms.Normalize(mean=[mean], std=[std])
