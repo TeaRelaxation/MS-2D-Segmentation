e7bcd5c166c93acf7b5fe279623717939e0d80d7
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-08-29 23:54:31 +0330

    Add wrapper to dataloader

---
+++ dataloaders/select_data.py
@@ -0,0 +1,14 @@
+from .ms import MSDataset
+from .transform import get_augmentor, get_normalizer
+
+
+def select_data(dataset_name, dataset_path):
+    train_data = None
+    val_data = None
+    train_augmentor = get_augmentor(mode="train")
+    test_augmentor = get_augmentor(mode="test")
+    if dataset_name == "MS":
+        normalizer = get_normalizer(mean=0.5, std=0.5)
+        train_data = MSDataset(root_dir=f"{dataset_path}/train", augmentor=train_augmentor, normalizer=normalizer)
+        val_data = MSDataset(root_dir=f"{dataset_path}/test", augmentor=test_augmentor, normalizer=normalizer)
+    return train_data, val_data
