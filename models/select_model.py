cc8b2a5f1339431877cb6462e2edb691c7fe13a3
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-08-30 15:13:01 +0330

    Add DeepLabV3Plus_ResNet34 model

--- models/select_model.py
+++ models/select_model.py
@@ -1,10 +1,18 @@
 from .unet import UNet
+import segmentation_models_pytorch as smp
 
 
 def select_model(model_name, n_classes):
     n_channels = 1
     if model_name == "UNet":
         return UNet(n_channels=n_channels, n_classes=n_classes, bilinear=False)
+    if model_name == "DeepLabV3Plus_ResNet34":
+        return smp.DeepLabV3Plus(
+            encoder_name="resnet34",
+            in_channels=n_channels,
+            classes=n_classes,
+            encoder_weights=None,
+        )
     else:
         return None
 
