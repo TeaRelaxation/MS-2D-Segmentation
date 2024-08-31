718eb5597d1b38de629e84bc67f23ec9c381313a
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-08-31 23:38:27 +0330

    Add dice focal combination

--- losses/dice_focal.py
+++ losses/dice_focal.py
@@ -3,19 +3,16 @@ import torch.nn as nn
 from .focal import FocalLoss
 
 
-class WCEDiceFocalLoss(nn.Module):
-    def __init__(self, class_weights, weight_ce=0.2, weight_dice=1.0, weight_focal=1.0):
-        super(WCEDiceFocalLoss, self).__init__()
-        self.weight_ce = weight_ce
+class DiceFocalLoss(nn.Module):
+    def __init__(self, weight_dice=0.5, weight_focal=0.5):
+        super(DiceFocalLoss, self).__init__()
         self.weight_dice = weight_dice
         self.weight_focal = weight_focal
-        self.cross_entropy = nn.CrossEntropyLoss(weight=class_weights)
         self.dice_loss = smp.losses.DiceLoss(mode="multiclass")
         self.focal_loss = FocalLoss(gamma=2.0)
 
     def forward(self, pred, target):
-        ce_loss = self.cross_entropy(pred, target)
         dice_loss = self.dice_loss(pred, target)
         focal_loss = self.focal_loss(pred, target)
-        combined_loss = (self.weight_ce * ce_loss) + (self.weight_dice * dice_loss) + (self.weight_focal * focal_loss)
+        combined_loss = (self.weight_dice * dice_loss) + (self.weight_focal * focal_loss)
         return combined_loss
--- losses/select_loss.py
+++ losses/select_loss.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import segmentation_models_pytorch as smp
 from .focal import FocalLoss
+from .dice_focal import DiceFocalLoss
 from .wce_dice import WCEDiceLoss
 from .wce_dice_focal import WCEDiceFocalLoss
 
@@ -32,6 +33,8 @@ def select_loss(loss_name, device):
         return smp.losses.DiceLoss(mode="multiclass")
     elif loss_name == "Focal":
         return FocalLoss(gamma=2.0)
+    elif loss_name == "DiceFocal":
+        return DiceFocalLoss()
     elif loss_name == "WCEDice":
         return WCEDiceLoss(class_weights=class_weights)
     elif loss_name == "WCEDiceFocal":
