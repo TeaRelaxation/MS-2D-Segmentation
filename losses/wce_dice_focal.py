30e06c43602b4995621ea004842ab058fb8cbdaf
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-09-02 00:17:09 +0330

    Add new weighting mechaninsm to combined loss

--- losses/wce_dice_focal.py
+++ losses/wce_dice_focal.py
@@ -17,5 +17,18 @@ class WCEDiceFocalLoss(nn.Module):
         ce_loss = self.cross_entropy(pred, target)
         dice_loss = self.dice_loss(pred, target)
         focal_loss = self.focal_loss(pred, target)
-        combined_loss = (self.weight_ce * ce_loss) + (self.weight_dice * dice_loss) + (self.weight_focal * focal_loss)
-        return combined_loss
+
+        ce_loss_value = ce_loss.item()  # 2
+        dice_loss_value = dice_loss.item()  # 4
+        focal_loss_value = focal_loss.item()  # 8
+        total_loss_value = ce_loss_value + dice_loss_value + focal_loss_value  # 14
+
+        epsilon = 1e-8
+        norm_ce_weight = total_loss_value / (ce_loss_value + epsilon)  # 7
+        norm_dice_weight = total_loss_value / (dice_loss_value + epsilon)  # 3.5
+        norm_focal_weight = total_loss_value / (focal_loss_value + epsilon)  # 1.75
+
+        combined_loss = (self.weight_ce * norm_ce_weight * ce_loss) + \
+                        (self.weight_dice * norm_dice_weight * dice_loss) + \
+                        (self.weight_focal * norm_focal_weight * focal_loss)
+        return combined_loss / 3
