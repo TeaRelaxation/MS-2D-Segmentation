4a294a69665aacd0196081934792e33a744b3b23
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-09-11 23:36:16 +0330

    Comment Elastic transform

--- dataloaders/transform.py
+++ dataloaders/transform.py
@@ -22,7 +22,7 @@ def get_augmentor(mode, height, width):
             A.VerticalFlip(p=0.5),
             A.Rotate(limit=10, p=0.5),
             A.OneOf([
-                A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
+                # A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
                 A.GridDistortion(p=0.5),
                 A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=1)
             ], p=0.8),
