511773521c8730b5e21ef6ea423b0740ac5e280a
Ali Sedaghi <ali_sedaghi@ymail.com>
2024-09-12 02:45:26 +0330

    Comment out new data aug

--- dataloaders/transform.py
+++ dataloaders/transform.py
@@ -19,16 +19,16 @@ def get_augmentor(mode, height, width):
         return A.Compose([
             get_padding(height, width),
             A.RandomCrop(height=height, width=width),
-            A.VerticalFlip(p=0.5),
+            A.HorizontalFlip(p=0.5),
             A.Rotate(limit=10, p=0.5),
-            A.OneOf([
-                # A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
-                A.GridDistortion(p=0.5),
-                A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=1)
-            ], p=0.8),
-            # A.CLAHE(p=0.8),
-            A.RandomBrightnessContrast(p=0.8),
-            A.RandomGamma(p=0.8),
+            # A.OneOf([
+            #     # A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
+            #     A.GridDistortion(p=0.5),
+            #     A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=1)
+            # ], p=0.8),
+            # # A.CLAHE(p=0.8),
+            # A.RandomBrightnessContrast(p=0.8),
+            # A.RandomGamma(p=0.8),
             ToTensorV2()  # HWC -> CHW (Only image not mask)
         ])
     elif mode == "test":
